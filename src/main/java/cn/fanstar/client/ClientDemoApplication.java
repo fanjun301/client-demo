d17978d8d46e901709a48b2cc63cec2c16f79b7e
fanjun <fanjun301@qq.com>
2018-02-25 20:34:36 +0800

    add for hystrix

--- src/main/java/cn/fanstar/client/ClientDemoApplication.java
+++ src/main/java/cn/fanstar/client/ClientDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ClientDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/cn/fanstar/client/remote/ServerDemoRemoteService.java
+++ src/main/java/cn/fanstar/client/remote/ServerDemoRemoteService.java
@@ -1,6 +1,7 @@
 package cn.fanstar.client.remote;
 
 import cn.fanstar.client.remote.feigns.ServerDemoFeign;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -20,9 +21,14 @@ public class ServerDemoRemoteService {
     private ServerDemoFeign serverDemoFeign;
 
 
+    @HystrixCommand(fallbackMethod = "helloHystrix")
     public String hello(){
         //TODO 远程访问异常编码
         return restTemplate.getForObject("http://server-demo/api/hello", String.class);
+    }
+
+    public String helloHystrix(){
+        return "greeting from client local hystrix";
     }
 
     public String helloFeign(){
